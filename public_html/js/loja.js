56ad7ad00315738c6f4b12444fdc12a844e3f4ce
Ramon Chiara <ramonchiara@gmail.com>
2017-09-10 21:45:35 -0300

    Carregando produtos via AJAX.

--- public_html/js/loja.js
+++ public_html/js/loja.js
@@ -1,8 +1,12 @@
 var app = angular.module('lojaApp', []);
 
-app.controller('lojaCtrl', function ($scope) {
-    
+app.controller('lojaCtrl', function ($scope, $http) {
+
     $scope.min = 0;
     $scope.max = 10000;
-    
+    $scope.produtos = [];
+
+    $http.get('produtos.json').then(function (response) {
+        $scope.produtos = response.data;
+    });
 });
