e70b802f00d30cc0d0a950fda34b37e289d1c8ae
Ramon Chiara <ramonchiara@gmail.com>
2017-09-10 22:26:35 -0300

    Atualizando o min e max range com o menor e maior preço obtido da lista.

--- public_html/js/loja.js
+++ public_html/js/loja.js
@@ -4,10 +4,21 @@ app.controller('lojaCtrl', function ($scope, $http) {
 
     $scope.min = 0;
     $scope.max = 10000;
+
+    $scope.minPreco = 0;
+    $scope.maxPreco = 10000;
+
     $scope.produtos = [];
 
     $http.get('produtos.json').then(function (response) {
         $scope.produtos = response.data;
+
+        var precos = response.data.map(function (p) {
+            return p.preco;
+        });
+
+        $scope.minPreco = Math.min.apply(null, precos);
+        $scope.maxPreco = Math.max.apply(null, precos);
     });
 
     $scope.filtro = function (value, index, array) {
